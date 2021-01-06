1920c96a24f4497142e82dd4d71254e07233154b
agusk3 <61266725+agusk3@users.noreply.github.com>
2021-01-06 17:35:26 +0700

    Create index.php

---
+++ logo/index.php
@@ -0,0 +1,18 @@
+<title> Tạo logo Wap - logo angrybird - logo androi - logo army - logo ngũ đế - logo rocket | FBVN.CF </title>
+
+<?php
+
+include 'head.php';?>
+
+<h2 class=phdr><span itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a title="Trang chủ" itemprop="url" href="/"><span itemprop="title"> Trang chủ </span></a></span><font color=#000> »</font><span itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a title="Tool Tiện Ích" itemprop="url" herf="/tools"><span itemprop="title">Tool Tiện Ích</span></a></span></h2><div class="phdr">LOGO HOT 2015</div><div class="list1"><a href="/tools/apk">Tạo Logo Android</a></div>
+
+<div class="list1"><a href="/tools/army">Tạo Logo Army</a></div><div class="list1"><a href="/tools/rocket">Tạo Logo Rocket</a></div>
+
+<div class="list1"><a href="/tools/angry">Tạo Logo Angry bird</a></div><div class="list1"><a href="/tools/nd">Tạo Logo ngũ đế</a></div>
+
+<?php
+
+include 'end.php';
+
+?>
+
